d54cb560f0e64171c29c491f0776d3274824cf53
Mrcococolar <gcy031295@163.com>
2016-12-24 20:41:33 +0800

    modify: 	examples/main_loop.c

--- examples/main_loop.c
+++ examples/main_loop.c
@@ -1,6 +1,48 @@
 #include "stm32f10x.h"
 
+int TimingDelay=0;
+
+void init_GPIO(){
+	GPIO_InitTypeDef GPIO_Initstructure;
+	GPIO_Initstructure.GPIO_Pin = GPIO_Pin_2;
+	GPIO_Initstructure.GPIO_Speed = GPIO_Speed_10MHz;
+	GPIO_Initstructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_Init(GPIOE,&GPIO_Initstructure);
+}
+
+void init_RCC(){
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOE, ENABLE);
+	SysTick_Config(72000);
+}
+
+void TimingDelay_Decrement(void)
+{
+    if (TimingDelay != 0x00)
+    {
+        TimingDelay--;
+    }
+}
+
+void delay_ms(int t){
+	TimingDelay = t;
+}
 
 int main(){
+	init_RCC();
+	init_GPIO();
+	while(1)
+	{
+		//GPIO_WriteBit(GPIOE, GPIO_Pin_2, Bit_SET);
+		GPIO_SetBits(GPIOE,GPIO_Pin_2);
+		delay_ms(500);
+		//GPIO_WriteBit(GPIOE, GPIO_Pin_2, Bit_RESET);
+		GPIO_ResetBits(GPIOE,GPIO_Pin_2);
+		delay_ms(500);
+	}
 	return 1;
+}
+
+void SysTick_Handler(void)
+{
+    TimingDelay_Decrement();
 }
